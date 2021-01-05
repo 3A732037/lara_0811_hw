e8aaabb5094a1f8f5845e728a68994b4b2c8e090
3a732037 <3a732037@gm.student.ncut.edu.tw>
2021-01-06 01:42:18 +0800

    建立專案

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('hello/{name?}',['as'=>'hello.index',function($name='Everybody'){
+return'Hello,'.$name;
+}]);
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admindashboard';
+    });
+});
+
+
+Route::get('/',function(){
+    return view('welcome');
+});
+Route::get('/',function(){
+    return redirect('welcome');
+});
